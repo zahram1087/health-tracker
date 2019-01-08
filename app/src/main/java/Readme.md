7c2613ae82b445f2ed2fd92365c792b1ca0a7ef3
Zahra <zahram1087@gmail.com>
2019-01-08 07:56:42 -0800

    first commit

---
+++ app/src/main/java/Readme.md
@@ -0,0 +1,4 @@
+Feature Tasks
+Finger Exercises
+One key part of health is building finger strength and endurance. On the main page, display a number and a button. The number should increase when the button is clicked.
+
---
+++ app/src/main/java/com/mohamed/health_tracker/MainActivity.java
@@ -0,0 +1,37 @@
+package com.mohamed.health_tracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    private void display(int number) {
+        TextView displayInteger = (TextView) findViewById(
+                R.id.editText);
+        displayInteger.setText("" + number);
+    }
+
+
+    int startNumber = 0;
+
+    //call the display method and passes the redefined startNumber
+    public void increaseInteger(View view) {
+        startNumber = startNumber + 1;
+        display(startNumber);
+
+    }
+
+// int num = 0;
+    // num = num+1;
+    //call this displayMessage
+    }
+
