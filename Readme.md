750d109ec2fdfbb2d64f83cc9e8df03bdeb2d37f
Zahra <zahram1087@gmail.com>
2019-01-09 08:44:38 -0800

    part2 done

--- Readme.md
+++ Readme.md
@@ -1,4 +1,6 @@
 Feature Tasks
 Finger Exercises
-One key part of health is building finger strength and endurance. On the main page, display a number and a button. The number should increase when the button is clicked.
+One key part of health is building finger strength and endurance. On the main page,
+display a number and a button. The number should increase when the button is clicked.
 
+source: https://stackoverflow.com/questions/4597690/android-timer-how-to
--- app/src/main/java/com/mohamed/health_tracker/MainActivity.java
+++ app/src/main/java/com/mohamed/health_tracker/MainActivity.java
@@ -1,38 +1,107 @@
 package com.mohamed.health_tracker;
 
+import android.icu.text.TimeZoneFormat;
 import android.nfc.Tag;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TimeFormatException;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import java.sql.Time;
+import java.text.DateFormat;
 import java.time.Clock;
+import java.util.Date;
+import java.util.Timer;
+
+import static java.lang.System.currentTimeMillis;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** FOR CLOCK FUNCTIONALITY */
+
+    /** reference the following code for my timer solution: source: https://stackoverflow.com/questions/4597690/android-timer-how-to CLOCK FUNCTIONALITY */
+
+    TextView timerView;
+    long initialTime = 0; //startTime
+    long currentTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-//        // get your ToggleButton
-//        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
-//
-//        // attach an OnClickListener
-//        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (isChecked) {
-//                    // The toggle is enabled
-//                } else {
-//                    // The toggle is disabled
-//                }
-//            }
-//        });
+        timerView = (TextView) findViewById(R.id.editText2);
+        final Button startPauseButton = findViewById(R.id.toggleButton);
+
+        startPauseButton.setText("Start");
+
+
+        startPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (startPauseButton.getText().equals("Pause")) {
+                    currentTime = System.currentTimeMillis() - initialTime + currentTime;
+                    stopWatchHandler.removeCallbacks(stopWatchRunner);
+                    startPauseButton.setText("Start");
+
+                } else {
+                    initialTime = System.currentTimeMillis();
+                    startPauseButton.setText("Pause");
+                    stopWatchHandler.postDelayed(stopWatchRunner, 0);
+                }
+
+            }
+        });
     }
+
+
+    Handler stopWatchHandler = new Handler();
+    Runnable stopWatchRunner = new Runnable() {
+        @Override
+        public void run() {
+            long millis = (System.currentTimeMillis() - initialTime + currentTime);
+            int seconds = (int)(millis/1000);
+            int minutes = seconds/60;
+            int hours = minutes/60;
+
+            millis = millis%1000;
+            seconds = seconds%60;
+            minutes = minutes%60;
+            hours = hours%24;
+
+            timerView.setText(String.format("%02d:%02d:%02d:%03d", hours, minutes, seconds, millis));
+            stopWatchHandler.postDelayed(this, 50);
+
+        }
+    };
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopWatchHandler.removeCallbacks(stopWatchRunner);
+        Button startStopButton = findViewById(R.id.toggleButton);
+        startStopButton.setText("Start");
+    }
+
+    public void onReset (View v) {
+        currentTime = 0;
+        stopWatchHandler.removeCallbacks(stopWatchRunner);
+        TextView time = findViewById(R.id.editText2);
+        time.setText("00:00:00:000");
+        Button startStop = findViewById(R.id.toggleButton);
+        startStop.setText("Start");
+    }
+
+
+
+
 
 /** FOR INCREASE FUNCTIONALITY */
     /** Called when the user taps the Send button */
@@ -50,7 +119,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     int startNumber = 0;
 
     //call the display method and passes the redefined startNumber
@@ -59,44 +127,6 @@ public class MainActivity extends AppCompatActivity {
         display(startNumber);
 
     }
-    /** FOR CLOCK FUNCTIONALITY */
 
-
-    public void nClickToggle(View view) {
-
-        // get your ToggleButton
-        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
-
-        // attach an OnClickChangeListener
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    TextView displayTime = (TextView) findViewById(R.id.editText2);
-
-                    displayTime.setText(Long.toString(System.currentTimeMillis()));
-
-                    //formating
-                    //continueing the time
-
-
-
-                } else {
-                    // The toggle is disabled
-                    Log.d("BT","BROKE");
-                    //STOPS IT AT CURRENT TIME
-                    TextView displayTime = (TextView) findViewById(R.id.editText2);
-
-//                    Log.d("time:", Long.toString(System.currentTimeMillis()));
-                    displayTime.setText(Long.toString(System.currentTimeMillis()));
-
-                }
-
-            }
-
-
-        });
-
-    }
 }
 
