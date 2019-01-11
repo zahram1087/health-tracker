b8b1b5ec06ff362092ca52de5c2f1cc55e4667a6
Zahra <zahram1087@gmail.com>
2019-01-10 18:10:39 -0800

    finalizing

--- app/src/main/java/com/mohamed/health_tracker/MainActivity.java
+++ app/src/main/java/com/mohamed/health_tracker/MainActivity.java
@@ -26,14 +26,18 @@ import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
-    /** FOR NOTIFICATION FUNCTIONALITY */
+    /**
+     * FOR NOTIFICATION FUNCTIONALITY
+     */
 
     private static final String CHANNEL_ID = "channelid";
 
     private int notificationId = 1;
 
 
-    /** FOR CAROUSEL FUNCTIONALITY */
+    /**
+     * FOR CAROUSEL FUNCTIONALITY
+     */
     CarouselView carouselView;
     int[] sampleImages = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4};
     String[] quote = {"You are amazing", "Keep giving your best", "Love yourself", "You are super!"};
@@ -99,72 +103,75 @@ public class MainActivity extends AppCompatActivity {
 
     /** FOR NOTIFICATION FUNCTIONALITY */
 
-    /** reference the following for notification build solution: source: //https://developer.android.com/training/notify-user/build-notification and https://gist.github.com/BrandonSmith/6679223*/
+    /**
+     * reference the following for notification build solution: source: //https://developer.android.com/training/notify-user/build-notification and https://gist.github.com/BrandonSmith/6679223
+     */
 
     //set the notification's content and channel using a NotificationCompat.Builder object
+    public void fireNotification() {
+        //user timer inspired by: https://stackoverflow.com/questions/4249542/run-a-task-every-x-minutes-with-windows-task-scheduler
 
-
-     public void fireNotification () {
-         //user timer inspired by: https://stackoverflow.com/questions/4249542/run-a-task-every-x-minutes-with-windows-task-scheduler
-
-
-         java.util.Timer time = new java.util.Timer();
-         time.schedule(new TimerTask() {
-             @Override
-             public void run() {
-                 NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
-                         .setSmallIcon(R.drawable.img1)
-                         .setContentTitle("Health Notifications")
-                         .setContentText("Drink Water")
-                         .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-
-                 notificationManager.notify(notificationId++, mBuilder.build());
-
-
-             }
-         },5000,5000);
-
-    /** reference the following for notification channel build method :source: https://developer.android.com/training/notify-user/channels*/
-
-    private void createNotificationChannel() {
-
-
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = CHANNEL_ID;
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
-
-    public void stopNotifications(View view){
 
         java.util.Timer time = new java.util.Timer();
-//        time.schedule(new TimerTask() {
-//            @Override
-//            public void run() {
-//                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
-//                        .setSmallIcon(R.drawable.img1)
-//                        .setContentTitle("Health Notifications")
-//                        .setContentText("Drink Water")
-//                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-//
-////                notificationManager.notify(notificationId++, mBuilder.build());
-//
-//                notificationManager.cancel(String.valueOf(MainActivity.this), notificationId);
-//
-//            }
-//        },5000,5000);
-        time.cancel();
+        time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.img1)
+                        .setContentTitle("Health Notifications")
+                        .setContentText("Drink Water")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
+
+                notificationManager.notify(notificationId++, mBuilder.build());
+
+
+            }
+        }, 5000, 5000);
     }
 
+        /** reference the following for notification channel build method :source: https://developer.android.com/training/notify-user/channels*/
 
-}
+        private void createNotificationChannel () {
+
+
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = CHANNEL_ID;
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        public void stopNotifications (View view){
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.img1)
+                    .setContentTitle("Health Notifications")
+                    .setContentText("Drink Water")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            Intent fireNotificationIntent = new Intent(this, NotificationReciever.class);
+
+            //at this point: include the extra information you will use in the notification (from the NotificationReciever Class)
+            fireNotificationIntent.putExtra("notification", mBuilder.build());
+            fireNotificationIntent.putExtra("notification_id", notificationId++);
+
+            //pending intent's job is to send out the above intent
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, fireNotificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            //Next is to schedule the pending intent using an alarm manager
+
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+            alarmManager.cancel(pendingIntent);
+        }
+
+    }
+
 
